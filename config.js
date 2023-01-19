a8f5d1f479821d0a4592e790fae9ac3042e5b962
Eroc123 <79434990+Eroc123@users.noreply.github.com>
2023-01-19 21:42:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'bd69cdd790565a78ca1cba9e91fc1dbf', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
